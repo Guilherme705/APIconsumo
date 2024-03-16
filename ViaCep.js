3e3f54381ca95a61c1cf3ab748f3d3eba8cad0ca
Guilherme705 <guilhermemuniz705@gmail.com>
2024-03-15 22:20:03 -0300

    criando arquivo JS individual para API viaCep

---
+++ ViaCep.js
@@ -0,0 +1,13 @@
+async function viaCepApi(){
+    const cep = document.getElementById('cep').value;
+    try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const data = await response.json();
+        console.log(data);
+        document.getElementById('logradouro').value = data.logradouro;
+        document.getElementById('bairro').value = data.bairro;
+        document.getElementById('localidade').value = data.localidade;
+    } catch (error) {
+        alert(error.message);
+    }
+}
