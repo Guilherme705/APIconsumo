1cc57ae6afec90a443281aa1419303ca204a7ca0
PCM <PCM@beauvallet.local>
2024-03-26 09:22:53 -0300

    finalizando o projeto

--- weatherApi.js
+++ weatherApi.js
@@ -2,7 +2,8 @@ async function weather(){
     const lat = document.getElementById('latitude').value;
     const long = document.getElementById('long').value;
     try {
-        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m`);
+        const response = await fetch
+        (`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m`);
         const data = await response.json();
         console.log(data);
         let = data.lenght
