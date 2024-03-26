0684731dadbf332df905dacfdb4ee2b8b43cf3c0
Guilherme705 <guilhermemuniz705@gmail.com>
2024-03-25 23:03:13 -0300

    adicionada funcionalidade para previsão do tempo com consumo de API

--- weatherApi.js
+++ weatherApi.js
@@ -5,6 +5,13 @@ async function weather(){
         const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m`);
         const data = await response.json();
         console.log(data);
+        let = data.lenght
+        document.getElementById('00').value = 'As 00:00 á temperatura será de ' + data.hourly.temperature_2m[0] + '°C';
+        document.getElementById('04').value = 'As 04:00 á temperatura será de ' + data.hourly.temperature_2m[4] + '°C';
+        document.getElementById('08').value = 'As 08:00 á temperatura será de ' + data.hourly.temperature_2m[8] + '°C';
+        document.getElementById('12').value = 'As 12:00 á temperatura será de ' + data.hourly.temperature_2m[12]+ '°C' ;
+        document.getElementById('16').value = 'As 16:00 á temperatura será de ' + data.hourly.temperature_2m[16]+ '°C';
+        document.getElementById('20').value = 'As 20:00 á temperatura será de ' + data.hourly.temperature_2m[20]+ '°C';
     }catch (error){
         alert(error.message);
     }
